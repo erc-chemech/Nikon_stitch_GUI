b76c9b7b2e36c9644a698f6780d47547587d05a2
yehmon01 <CHJoshuaYeh@gmail.com>
2019-02-05 13:26:48 +0100

    added gpu functionality

--- stitching fcns/gpu_check.m
+++ stitching fcns/gpu_check.m
@@ -1,5 +1,10 @@
 % Determine GPU capabilities. If it is sufficient, use GPU device.
 function [dev,flag4]=gpu_check()
+    if license('test','Distrib_Computing_Toolbox')==0
+        flag4=0;
+        dev=nan;
+        return
+    end
     if ~(parallel.gpu.GPUDevice.isAvailable)
         fprintf(['\n\t**GPU does not have a compute capability of 1.3 or '...
                  'greater.']);
